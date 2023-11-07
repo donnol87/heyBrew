975e5ebbbc5e106d204f5d2b67b95d3b043efaf6
donnol87 <donn@donnolochlainn.com>
2023-11-07 19:44:52 +0000

    added create element to page and corrected button outside of form bug

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ let input = document.getElementById("input");
 let msg = document.getElementById("msg");
 let ingredients = document.getElementById("ingredients");
 
+let data = {};
+
 // build an event listener to prevent the default behaviour
 //also create a variable a function name formValidation
 
@@ -22,5 +24,27 @@ form.addEventListener("submit", (e) => {
     } else {
       console.log("successs");
       msg.innerHTML = "";
+      acceptData();
     }
+  };
+
+// here we area accepting data from input fields and we added the acceptData() function to the above function
+let acceptData = () => {
+    data["text"] = input.value;
+    console.log(data);
+    createPost();
+};
+
+// new function for creating template literals
+
+let createPost = () => {
+    ingredients.innerHTML += `
+    <div>
+    <p>${data.text}</p>
+    <span class="options">
+      <i onClick="editPost(this)" class="fas fa-edit"></i>
+      <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+    </span>
+  </div>`;
+  input.value = "";
   };
